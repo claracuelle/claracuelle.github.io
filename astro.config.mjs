ecc99698bf01c345cd0fde4450d0375b1a88d8d9
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-07-02 14:24:45 +0200

    🐛 Test env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { getEnv } from 'astro/env/runtime';
 
 export default defineConfig({
   site: 'https://claracuelle.github.io',
