1b5907e4987d07dd4b4f205cddc8556fe1c85c2c
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-07-02 10:09:00 +0200

    ✨ Copy paste mail header 🐛 Base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { getEnv } from 'astro/env/runtime';
 
 export default defineConfig({
   site: 'https://claracuelle.github.io',
-  base: 'portfolio',
+  base: import.meta.env.PROD ? 'portfolio' : '',
   integrations: [tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,26 @@ export default {
         migra: ['Migra-Extrabold', 'sans-serif'],
         manrope: ['Manrope-Regular', 'sans-serif'],
       },
+      colors: {
+        black: {
+          DEFAULT: '#151515',
+          hover: '#585858',
+          pressed: '#424242',
+          disabled: '#B0B0B0',
+        },
+        'expo-primary': {
+          DEFAULT: '#0085FF',
+          hover: '#3EA2FF',
+          pressed: '#0069E3',
+          disabled: '#C4E1FC',
+        },
+        'expo-secondary': {
+          DEFAULT: '#CCF0FB',
+          hover: '#E5F9FF',
+          pressed: '#B1E9FA',
+          disabled: '#D2E1E6',
+        },
+      },
     },
   },
   plugins: [],
