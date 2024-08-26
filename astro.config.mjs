d87a9fd5c97976afcf829596cd0fbfc27f920d5b
Lucas Laplanche <36164636+LucasLpc@users.noreply.github.com>
2024-08-26 09:05:32 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://claracuelle.github.io',
   base: 'portfolio',
-  integrations: [tailwind(), svelte()],
-  compressHTML: true
+  integrations: [tailwind()],
+  compressHTML: true,
 });
